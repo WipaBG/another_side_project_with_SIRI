b86fb02cbed03c065462101acce73a6b2188c386
Aleksandar Nyagolov <aleksandar_niagolov@abv.bg>
2026-05-09 11:28:40 +0300

    fix deployment

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = Number(process.env.PORT ?? 3000);
-  const host = '127.0.0.1';
 
-  await app.listen(port, host);
-  console.log(`Summer villa Siri API running on http://${host}:${port}`);
+  await app.listen(port);
+  console.log(`Summer villa Siri API running on http://localhost:${port}`);
 }
 
 bootstrap();
